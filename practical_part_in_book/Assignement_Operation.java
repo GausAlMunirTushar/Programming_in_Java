91b0f008a7d9982b76ef8f7b7eba0e67577eacf2
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-07 23:55:48 +0600

    some practical file added

---
+++ practical_part_in_book/Assignement_Operation.java
@@ -0,0 +1,7 @@
+package practical_part_in_book;
+
+public class Assignement_Operation {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ practical_part_in_book/twoNumberSum.java
@@ -0,0 +1,8 @@
+package practical_part_in_book;
+
+public class twoNumberSum {
+public static void main(String[] args) {
+    
+}
+
+}
