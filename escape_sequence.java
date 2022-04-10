b4a8fa71562f026191eb88983b89831afbbb9fc7
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-10 17:35:48 +0600

    escape sequence file added

---
+++ escape_sequence.java
@@ -0,0 +1,21 @@
+public class escape_sequence {
+   public static void main(String[] args) {
+        System.out.println("Hello\tWorld");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\\World");
+        System.out.println("Hello\bWorld");
+        System.out.println("Hello\fWorld");
+        System.out.println("Hello\rWorld");
+        System.out.println("Hello\u0041World");
+        System.out.println("Hello\u0041\u0042\u0043World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047\u0048World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047\u0048\u0049World");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047\u0048\u0049\u004AWorld");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047\u0048\u0049\u004A\u004BWorld");
+        System.out.println("Hello\u0041\u0042\u0043\u0044\u0045\u0046\u0047\u0048\u0049\u004A\u004B\u004C
+   } 
+}
