f33a3b8e3c53cbd39efec9793ec67ec932d4b66f
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-14 17:25:59 +0600

    hello.java file added some code

---
+++ hello.java
@@ -0,0 +1,8 @@
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Sachin";
+        int age = 30;
+        System.out.println("My name is " + name + " and I am " + age + " years old");
+    }
+}
