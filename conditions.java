46482b33d25a4e7ab476e9613e7c07575b51f966
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-16 14:27:06 +0600

    for loops pratices

--- conditions.java
+++ conditions.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        /* System.out.println("Enter a number: ");
         int num1 = sc.nextInt();
         System.out.println("Enter another number: ");   
         int num2 = sc.nextInt();
@@ -15,6 +15,29 @@ public class conditions {
         }
         else{
             System.out.println("Num1 is less");
+        } */
+        System.out.println("Enter a number: ");
+        int button = sc.nextInt();
+        /* if(button == 1){
+            System.out.println("Button 1 is pressed");
+        }
+        else if(button == 2){
+            System.out.println("Button 2 is pressed");
+        }
+        else if(button == 3){
+            System.out.println("Button 3 is pressed");
+        }
+        else{
+            System.out.println("Button is not pressed");
+        } */
+        switch(button){
+            case 1: System.out.println("button 1 is pressed");
+            break;
+            case 2: System.out.println("button 2 is pressed");
+            break;
+            case 3: System.out.println("button 3 is pressed");
+            break;
+            default: System.out.println("button is not pressed");
         }
     }
 }
