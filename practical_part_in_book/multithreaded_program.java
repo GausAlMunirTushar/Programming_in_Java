475452c1ee8933cfe35c01444ab21086736a531b
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-19 17:30:06 +0600

    practical part all program file added

---
+++ practical_part_in_book/multithreaded_program.java
@@ -0,0 +1,5 @@
+package practical_part_in_book;
+
+public class multithreaded_program {
+    
+}
---
+++ practical_part_in_book/program_4.java
@@ -0,0 +1,6 @@
+package practical_part_in_book;
+
+public class program_4 {
+    // Write and Excute Java Program Using Class, Object Mehthod and Constrcutor
+    
+}
