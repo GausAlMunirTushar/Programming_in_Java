a0b7e9f80de4f2877340c76d3694fd9cbf66e44d
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-05-15 10:57:37 +0600

    file added

--- practice_everything.java
+++ practice_everything.java
@@ -1,7 +1,9 @@
+import java.net.http.HttpResponse.BodySubscribers;
+
 public class practice_everything {
     public static void main(String[] args) {
         // hello world string print
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
         // This is Comments
 
         /* 
@@ -18,7 +20,31 @@ public class practice_everything {
         *
         * Amazing Facts About Java  - https://www.geeksforgeeks.org/amazing-facts-about-java/
         */
-        */
-        
+        // Variables
+        // int firtsNumber = 1;
+        // System.out.println(firtsNumber);
+
+        // Variables with different types
+        // Data Types in Java
+            /*     int number = 1;
+                double decimalNumber = 1.5;
+                char character = 'a';
+                boolean booleanValue = true;
+                byte marks = 90;
+                short age = 25;
+                long phoneNumber = 9876543210L;
+                float salary = 25000.00F;
+                String name = "John";
+                String lastName = "Doe";
+                double pi = 3.14; */
+
+        // string
+        // String a = "Gaus is the best";
+        // String b = "Tushar not a best";
+        // System.out.println(b.contains("not"));
+
+        // arrays
+        int[] marks = new int[] {90, 80, 70};
+        System.out.println(marks[0]);
     }
 }
