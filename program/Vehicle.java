22d776d371ddab51d0c06142c689a938ba3a35f0
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-07-20 20:45:27 +0600

    basic practices

---
+++ program/Vehicle.java
@@ -0,0 +1,10 @@
+package program;
+
+public class Vehicle {
+    
+}
+
+class Car extends  Vehicle {
+ 
+    
+}
---
+++ programs.java
@@ -0,0 +1,28 @@
+public class programs {
+    public static void main(String[] args) {
+        // our first proram
+        System.out.println("hello world");
+        // Variables
+        String name = "Gaus Al Munir Tushar";
+        int age = 20;
+        String friend = "Gias";
+        String newFriend = friend;
+
+     /*   primitive data types
+            byte age = 20;
+            int phone = 343434;
+            long phone2 = 349439473947394L;
+            flot pi = 3.14F;
+            char at = '@';
+            boolean isAdult = false; 
+        Non Primitive Data Types
+
+    */
+    // Strings 
+    String userName = name;
+    System.out.println(name.length());
+    String a = "hello";
+    String a2 = a.replace('e','t');
+        System.out.println(a2);
+    }
+}
